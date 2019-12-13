6d143609a5ea5cbaab7262dae0a8d2dd63ea2db8
Chris Larsen <3384850+derplarsen@users.noreply.github.com>
2019-12-13 18:56:06 -0500

    Update runOnTargetServer.sh

--- runOnTargetServer.sh
+++ runOnTargetServer.sh
@@ -5,4 +5,4 @@ if [ $disableSEYN == "y" ]; then sudo setenforce Permissive; fi
 echo 'SELinux has been temporarily disabled (until Reboot). If you want to permanently disable, you need to set SELINUX to disabled in /etc/selinux/config'
 read -p 'Ready to install the packages? [y/n]' installRpmsYN
 if [ $installRpmsYN == "y" ]; then cd packages; sudo rpm -ivh *.rpm; fi
-echo 'Packages all installed. Moving on to enabling Systemd for key components'
+echo 'Packages all installed. Move on to edit configs and enable and start Systemd things'
