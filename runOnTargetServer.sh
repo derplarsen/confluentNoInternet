8f036a35e37f3a056713b903388d986c5286526f
Chris Larsen <3384850+derplarsen@users.noreply.github.com>
2019-12-13 20:21:49 -0500

    Update runOnTargetServer.sh

--- runOnTargetServer.sh
+++ runOnTargetServer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo 'Good Job'
 read -p 'Is it OK to disable SELinux? [y/n]' disableSEYN
 if [ $disableSEYN == "y" ]; then sudo setenforce Permissive; fi
 echo 'SELinux has been temporarily disabled (until Reboot). If you want to permanently disable, you need to set SELINUX to disabled in /etc/selinux/config'
